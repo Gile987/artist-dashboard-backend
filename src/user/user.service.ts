a8eff4527f094e1a4c2219f7595b145fa387cfc6
gile987 <mladen.reljic87@gmail.com>
2025-08-09 13:31:37 +0200

    feat: implement user module with Prisma integration and update auth service

---
+++ src/user/user.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma.service';
+import { User } from '@prisma/client';
+
+@Injectable()
+export class UserService {
+  constructor(private prisma: PrismaService) {}
+
+  async findAll(): Promise<User[]> {
+    return await this.prisma.user.findMany();
+  }
+}
