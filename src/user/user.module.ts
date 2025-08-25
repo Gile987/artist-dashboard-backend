b59a7df6dff195517a522fa0434f47bc0181d481
gile987 <mladen.reljic87@gmail.com>
2025-08-25 16:14:46 +0200

    feat: add user deletion functionality for admins and update README

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
 import { UserService } from './user.service';
 import { UserController } from './user.controller';
 import { PrismaService } from '../prisma.service';
 
 @Module({
+  imports: [
+    JwtModule.register({
+      secret: 'your_jwt_secret', // Must match AuthModule secret
+      signOptions: { expiresIn: '1d' },
+    }),
+  ],
   providers: [UserService, PrismaService],
   controllers: [UserController],
   exports: [UserService],
